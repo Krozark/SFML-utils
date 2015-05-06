bd5d283a27bb303d5274f421035948019c38b945
Krozark <maxime.barbier1991@gmail.com>
2015-05-07 00:31:36 +0200

    clean build

--- cmake/macro.cmake
+++ cmake/macro.cmake
@@ -55,11 +55,7 @@ endmacro()
 macro(deploy)
 	cmake_parse_arguments(THIS "" "" "FILES;DESTINATION" ${ARGN})
 	
-	message(Deploy : ${THIS_FILES})
-	
 	file(GLOB DEPLOY_FILES_AND_DIRS "${THIS_FILES}")
-	
-	message(Deploy : ${DEPLOY_FILES_AND_DIRS})
 	
 	set(FILES_TO_DEPLOY "")
 	set(DIRS_TO_DEPLOY "")
@@ -67,10 +63,8 @@ macro(deploy)
 	foreach(ITEM ${DEPLOY_FILES_AND_DIRS})
 		IF( IS_DIRECTORY "${ITEM}" )
 			LIST( APPEND DIRS_TO_DEPLOY "${ITEM}" )
-			message(DIR ${ITEM})
 		ELSE()
 			LIST( APPEND FILES_TO_DEPLOY "${ITEM}" )
-			message(file ${ITEM})
 		ENDIF()
 	endforeach()
 	INSTALL( FILES ${FILES_TO_DEPLOY} DESTINATION ${THIS_DESTINATION} )
