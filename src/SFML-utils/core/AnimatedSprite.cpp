7169f9e9304fc8f70769af704edca43a8ad6349a
Krozark <maxime.barbier1991@gmail.com>
2015-07-29 20:56:42 +0200

    change AnimatedSprite heritanc class  to extand from sf::Sprite. This allow down casting for Layers

--- src/SFML-utils/core/AnimatedSprite.cpp
+++ src/SFML-utils/core/AnimatedSprite.cpp
@@ -20,6 +20,7 @@ namespace sfutils
             _elapsed = sf::Time::Zero;
             _currentFrame = 0;
             setFrame(0,true);
+            setTexture(*animation->getTexture());
         }
     }
 
@@ -85,14 +86,6 @@ namespace sfutils
         setFrame(_currentFrame,true);
     }
 
-    void AnimatedSprite::setColor(const sf::Color& color)
-    {
-        _vertices[0].color = color;
-        _vertices[1].color = color;
-        _vertices[2].color = color;
-        _vertices[3].color = color;
-    }
-
     void AnimatedSprite::update(const sf::Time& deltaTime)
     {
         if(_status == Playing and _animation)
@@ -126,35 +119,10 @@ namespace sfutils
         if(_animation)
         {
             sf::IntRect rect = _animation->getRect(index);
-
-            _vertices[0].position = sf::Vector2f(0.f, 0.f);
-            _vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
-            _vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
-            _vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
-
-            float left = static_cast<float>(rect.left);
-            float right = left + static_cast<float>(rect.width);
-            float top = static_cast<float>(rect.top);
-            float bottom = top + static_cast<float>(rect.height);
-
-            _vertices[0].texCoords = sf::Vector2f(left, top);
-            _vertices[1].texCoords = sf::Vector2f(left, bottom);
-            _vertices[2].texCoords = sf::Vector2f(right, bottom);
-            _vertices[3].texCoords = sf::Vector2f(right, top);
+            setTextureRect(rect);
         }
 
         if(resetTime)
             _elapsed = sf::Time::Zero;
     }
-
-    void AnimatedSprite::draw(sf::RenderTarget& target,sf::RenderStates states) const
-    {
-        if (_animation and _animation->_texture)
-        {
-            states.transform *= getTransform();
-            states.texture = _animation->_texture;
-            target.draw(_vertices, 4, sf::Quads, states);
-        }
-    }
-    
 }
