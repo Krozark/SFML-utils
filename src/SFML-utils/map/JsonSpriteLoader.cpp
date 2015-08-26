0ce0eea7abc6fcc3324e7a2a26e6ca23bb99edeb
Krozark <maxime.barbier1991@gmail.com>
2015-08-26 22:30:47 +0200

    add spriteSheet parsing

---
+++ src/SFML-utils/map/JsonSpriteLoader.cpp
@@ -0,0 +1,15 @@
+#include <SFML-utils/map/JsonSpriteLoader.hpp>
+
+#include <utils/json/Driver.hpp>
+
+namespace sfutils
+{
+    namespace map
+    {
+        std::unordered_map<std::string,sf::IntRect> parse(const std::string& filename)
+        {
+            std::unordered_map<std::string,sf::IntRect> res;
+            return res;
+        }
+    }
+}
