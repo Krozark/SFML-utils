1a085b80770a5a1e99c3bfe53a19b872e7b55071
Krozark <maxime.barbier1991@gmail.com>
2015-10-18 20:55:03 +0200

    add initialData

---
+++ include/SFML-utils/map/models/InitialData.hpp
@@ -0,0 +1,11 @@
+#ifndef SFUTILS_MAP_INITIALDATA_HPP
+#define SFUTILS_MAP_INITIALDATA_HPP
+
+namespace sfutils
+{
+    namespace map
+    {
+        bool loadInitialData();
+    }
+}
+#endif
---
+++ src/SFML-utils/map/models/InitialData.cpp
@@ -0,0 +1,15 @@
+#include <SFML-utils/map/models/InitialData.hpp>
+
+#include <SFML-utils/map/models/GeometryModel.hpp>
+#include <SFML-utils/map/models/LayerTypeModel.hpp>
+
+namespace sfutils
+{
+    namespace map
+    {
+        bool loadInitialData()
+        {
+           return false; 
+        }
+    }
+}
