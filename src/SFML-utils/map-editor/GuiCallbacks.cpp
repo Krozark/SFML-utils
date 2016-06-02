ecace2dfa374e7877222cdb7c9c3e469a2af108b
Krozark <maxime.barbier1991@gmail.com>
2016-06-02 22:47:49 +0200

    mov collbacks into a separate file

---
+++ src/SFML-utils/map-editor/GuiCallbacks.cpp
@@ -0,0 +1,399 @@
+#include <SFML-utils/map-editor/Gui.hpp>
+#include <SFML-utils/map-editor/Editor.hpp>
+
+#include <SFML-utils/cegui/GuiManager.hpp>
+#include <SFML-utils/cegui/DialogBox.hpp>
+
+#include <utils/string.hpp>
+
+#include <iostream>
+
+
+namespace sfutils
+{
+    namespace editor
+    {
+
+        ////////////////////// PRIVATE ///////////////////
+
+        ////File
+        bool Gui::_event_menuBar_file_new()
+        {
+            std::list<std::string> list;
+            for(auto& map :  sfutils::map::GeometryModel::all())
+            {
+                list.emplace_back(map->name);
+            }
+
+            sfutils::cegui::DialogBox::getStringAndItem(
+                _root,
+                "Create a new map",
+                "Enter the title, and choose the tile format",
+                list,
+                [this](const std::string& title,const std::string& geometry){
+                    for(auto& geo :  sfutils::map::GeometryModel::all())
+                    {
+                        if(geo->name == geometry)
+                        {
+                            sfutils::map::MapModel::pointer tmp = sfutils::map::MapModel::create();
+                            tmp->name = title;
+                            tmp->geometry = geo;
+                            tmp->scale = 50;
+                            tmp->areaWidth = 10;
+                            tmp->areaHeight = 10;
+                            tmp->save();
+
+                            this->_owner.setMap(tmp);
+                            break;
+                        }
+                    }
+
+                });
+            return true;
+        }
+
+        bool Gui::_event_menuBar_file_open()
+        {
+            std::list<std::string> list;
+            for(auto& map :  sfutils::map::MapModel::all())
+            {
+                list.emplace_back(map->name);
+            }
+
+            sfutils::cegui::DialogBox::getItem(
+                   _root,
+                   "Map to load",
+                   "Choose the map to load",
+                   list,
+                   [this](const std::string& value){
+                       for(auto& map :  sfutils::map::MapModel::all())
+                       {
+                           if(map->name == value)
+                           {
+                               this->_owner.setMap(map);
+                               break;
+                           }
+                       }
+
+                   });
+
+            return true;
+        }
+
+        bool Gui::_event_menuBar_file_save()
+        {
+            std::cout<<"MenuBar/File/Menu/Save clicked"<<std::endl;
+
+            CEGUI::Window* box = _root->getChild("MenuBar/Title");
+            std::string title = box->getText().c_str();
+            _owner.getMap()->name = title;
+            _owner.getMap()->save();
+
+
+            return true;
+        }
+
+        ////Edit
+        bool Gui::_event_menuBar_edit_undo()
+        {
+            std::cout<<"MenuBar/Edit/Menu/Undo clicked"<<std::endl;
+            return true;
+        }
+        bool Gui::_event_menuBar_edit_redo()
+        {
+            std::cout<<"MenuBar/Edit/Menu/Redo clicked"<<std::endl;
+            return true;
+        }
+
+        //Map
+        bool Gui::_event_menuBar_map_resize()
+        {
+            std::stringstream ss;
+            ss<<_owner.getMap()->scale.getValue()<<" "
+                <<_owner.getMap()->areaHeight.getValue()<<" "
+                <<_owner.getMap()->areaWidth.getValue();
+
+            sfutils::cegui::DialogBox::getString(
+                 _root,
+                 "Change map size",
+                 "Choose a new size\nScale Height Width",
+                 [this](const std::string& size){
+                      std::cout<<"size : "<<size<<std::endl;
+                      auto split = utils::string::split(size," ");
+                      _owner.getMap()->scale = std::stoi(split[0]);
+                      _owner.getMap()->areaHeight = std::stoi(split[1]);
+                      _owner.getMap()->areaWidth = std::stoi(split[2]);
+
+                      _owner.reloadMap();
+                 },
+                 [](){},
+                 ss.str(),
+                 "\\d(\\d)* \\d(\\d)* \\d(\\d)*"
+                 );
+            return true;
+        }
+
+        bool Gui::_event_menuBar_map_shape()
+        {
+            std::list<std::string> list;
+            for(auto& map :  sfutils::map::GeometryModel::all())
+            {
+                list.emplace_back(map->name);
+            }
+
+            sfutils::cegui::DialogBox::getItem(
+                   _root,
+                   "Change map shape",
+                   "Choose the new geometry",
+                   list,
+                   [this](const std::string& geometry){
+                       for(auto& geo :  sfutils::map::GeometryModel::all())
+                       {
+                           if(geo->name == geometry)
+                           {
+                               this->_owner.getMap()->geometry = geo;
+                               this->_owner.reloadMap();
+                               break;
+                           }
+                       }
+                   });
+            return true;
+        }
+
+        bool Gui::_event_menuBar_map_position()
+        {
+            std::cout<<"MenuBar/Map/Menu/Position clicked"<<std::endl;
+            return true;
+        }
+
+        ////LeftPanel
+        bool Gui::_event_leftPanel_texture_selected(CEGUI::Listbox* box)
+        {
+            CEGUI::ListboxItem* item = box->getFirstSelectedItem();
+            if(item)
+            {
+                std::cout<<"_event_leftPanel_texture_selected : "<<item->getText().c_str()<<std::endl;
+            }
+
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_changed(const std::string& name)
+        {
+            std::cout<<"_event_leftPanel_tab_changed"<<std::endl;
+            std::cout<<"_event_leftPanel_tab_changed :"<<name<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_NPC_add()
+        {
+            std::cout<<"_event_leftPanel_tab_NPC_add()"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_NPC_remove()
+        {
+            std::cout<<"_event_leftPanel_tab_NPC_remove()"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_NPC_selected(CEGUI::Listbox* box)
+        {
+            CEGUI::ListboxItem* item = box->getFirstSelectedItem();
+            if(item)
+            {
+                std::cout<<"_event_leftPanel_tab_NPC_selected : "<<item->getText().c_str()<<std::endl;
+            }
+
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_creature_add()
+        {
+            std::cout<<"_event_leftPanel_tab_creature_add()"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_creature_remove()
+        {
+            std::cout<<"_event_leftPanel_tab_creature_remove()"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_creature_selected(CEGUI::Listbox* box)
+        {
+            CEGUI::ListboxItem* item = box->getFirstSelectedItem();
+            if(item)
+            {
+                std::cout<<"_event_leftPanel_tab_creature_selected : "<<item->getText().c_str()<<std::endl;
+            }
+
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_bulding_add()
+        {
+            std::cout<<"_event_leftPanel_tab_bulding_add()"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_bulding_remove()
+        {
+            std::cout<<"_event_leftPanel_tab_bulding_remove()"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_leftPanel_tab_bulding_selected(CEGUI::Listbox* box)
+        {
+            CEGUI::ListboxItem* item = box->getFirstSelectedItem();
+            if(item)
+            {
+                std::cout<<"_event_leftPanel_tab_bulding_selected : "<<item->getText().c_str()<<std::endl;
+            }
+
+            return true;
+        }
+        
+        //void Gui::_event_top_titleChanged();
+        
+        bool Gui::_event_miniMap_zoom(float value)
+        {
+            if(value > 0)
+            {
+                _owner.setMiniMapZoom(value);
+            }
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_layer_selected()
+        {
+            CEGUI::ListboxItem* item = _layerList->getFirstSelectedItem();
+            if(item)
+            {
+                std::cout<<"_event_rightPanel_layer_selected : "<<item->getText().c_str()<<std::endl;
+            }
+
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_layers_add()
+        {
+            std::cout<<"_event_rightPanel_layers_add"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_layers_up()
+        {
+            CEGUI::ListboxItem* item = _layerList->getFirstSelectedItem();
+            if(item == nullptr)
+            {
+                return true;
+            }
+
+            unsigned int size = _layerList->getItemCount();
+            unsigned int index = _layerList->getItemIndex(item);
+            if(size == 1 or index == 0) //already on top
+            {
+                return true;
+            }
+
+            sfutils::map::LayerModel* layer = static_cast<sfutils::map::LayerModel*>(item->getUserData());
+            assert(layer);
+
+            int from = layer->zBuffer.getValue();
+            if(_owner.requestMoveLayer(from,from+1) == false)
+            {
+                return true;
+            }
+
+             CEGUI::ListboxItem* itemTop = _layerList->getListboxItemFromIndex(index -1);
+             assert(itemTop);
+
+            //remove move item
+            item->setAutoDeleted(false);
+            _layerList->removeItem(item);
+
+            //readd it at new place
+             _layerList->insertItem(item,itemTop);
+            item->setAutoDeleted(true);
+
+
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_layers_down()
+        {
+            CEGUI::ListboxItem* item = _layerList->getFirstSelectedItem();
+            if(item == nullptr)
+            {
+                return true;
+            }
+
+            unsigned int size = _layerList->getItemCount();
+            unsigned int index = _layerList->getItemIndex(item);
+
+            if(size == 1 or index == size - 1) //already on bottom
+            {
+                return true;
+            }
+            
+            sfutils::map::LayerModel* layer = static_cast<sfutils::map::LayerModel*>(item->getUserData());
+            assert(layer);
+
+            int from = layer->zBuffer.getValue();
+            if(_owner.requestMoveLayer(from,from-1) == false)
+            {
+                return true;
+            }
+
+
+            CEGUI::ListboxItem* itemBottom = _layerList->getListboxItemFromIndex(index + 1);
+            assert(itemBottom);
+
+            //remove move item
+            itemBottom->setAutoDeleted(false);
+            _layerList->removeItem(itemBottom);
+
+            //readd it at new place
+            _layerList->insertItem(itemBottom,item);
+            itemBottom->setAutoDeleted(true);
+
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_layers_remove()
+        {
+            std::cout<<"_event_rightPanel_layers_remove"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_tab_changed(const std::string& name)
+        {
+            std::cout<<"_event_rightPanel_tab_changed("<<name<<")"<<std::endl;
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_tab_brush_selected(CEGUI::Listbox* box)
+        {
+            CEGUI::ListboxItem* item = box->getFirstSelectedItem();
+            if(item)
+            {
+                std::cout<<"_event_rightPanel_tab_brush_selected : "<<item->getText().c_str()<<std::endl;
+            }
+
+            return true;
+        }
+
+        bool Gui::_event_rightPanel_tab_NPC_selected(CEGUI::Listbox* box)
+        {
+            CEGUI::ListboxItem* item = box->getFirstSelectedItem();
+            if(item)
+            {
+                std::cout<<"_event_rightPanel_tab_NPC_selected : "<<item->getText().c_str()<<std::endl;
+            }
+
+            return true;
+        }
+
+    }
+}
